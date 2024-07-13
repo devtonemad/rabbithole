dd01740fd5569d820e4aac635201c6e223288e8c
Matthias Oneisz <moneisz@gnubis.de>
2024-07-13 14:35:49 +0200

    feature for sending to exchange and queues and streams with headers and routing key possible

--- src/main/java/de/gnubis/rabbithole/MessageController.java
+++ src/main/java/de/gnubis/rabbithole/MessageController.java
@@ -1,11 +1,15 @@
 package de.gnubis.rabbithole;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MessageController {
@@ -22,8 +26,29 @@ public class MessageController {
     }
 
     @PostMapping("/send")
-    public String sendMessage(@RequestParam("queueName") String queueName, @RequestParam("message") String message) {
-        rabbitMQSender.send(queueName, message);
+    public String sendMessage(@RequestParam("destinationType") String destinationType,
+                              @RequestParam("destination") String destination,
+                              @RequestParam("routingKey") String routingKey,
+                              @RequestParam("message") String message,
+                              @RequestParam("headers") String headers) {
+        Map<String, Object> headersMap = new HashMap<>();
+        if (!headers.isEmpty()) {
+            String[] headersArray = headers.split(",");
+            for (String header : headersArray) {
+                String[] keyValue = header.split(":");
+                if (keyValue.length == 2) {
+                    headersMap.put(keyValue[0].trim(), keyValue[1].trim());
+                }
+            }
+        }
+
+        if (destinationType.equals("queue")) {
+            rabbitMQSender.sendToQueue(destination, message);
+        } else if (destinationType.equals("exchange")) {
+            rabbitMQSender.sendToExchange(destination, routingKey, message, headersMap);
+        } else if (destinationType.equals("stream")) {
+            rabbitMQSender.sendToStream(destination, message);
+        }
         return "redirect:/send";
     }
 
--- src/main/java/de/gnubis/rabbithole/RabbitMQSender.java
+++ src/main/java/de/gnubis/rabbithole/RabbitMQSender.java
@@ -1,8 +1,10 @@
 package de.gnubis.rabbithole;
-
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Map;
 
 @Service
 public class RabbitMQSender {
@@ -10,7 +12,20 @@ public class RabbitMQSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public void send(String queueName, String message) {
+    public void sendToQueue(String queueName, String message) {
         rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    public void sendToExchange(String exchange, String routingKey, String message, Map<String, Object> headers) {
+        MessageProperties messageProperties = new MessageProperties();
+        if (headers != null) {
+            headers.forEach(messageProperties::setHeader);
+        }
+        Message amqpMessage = new Message(message.getBytes(), messageProperties);
+        rabbitTemplate.send(exchange, routingKey, amqpMessage);
+    }
+
+    public void sendToStream(String stream, String message) {
+        sendToQueue(stream, message);
     }
 }
